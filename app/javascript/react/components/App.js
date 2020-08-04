20a6645366d0874150273ece29478b9425f8e2b9
flashnej <flashnej@gmail.com>
2020-08-04 12:53:22 -0400

    created show page

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom"
 
 import HomePage from "./HomePage"
 import AddABagForm from "./AddABagForm"
+import BagShowPage from "./BagShowPage"
 
 export const App = (props) => {
   return (
@@ -10,6 +11,7 @@ export const App = (props) => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/addABag" component={AddABagForm} />
+        <Route exact path="/bag/:id" component={BagShowPage} />
       </Switch>
     </BrowserRouter>
   )
--- app/javascript/react/components/BagTile.js
+++ app/javascript/react/components/BagTile.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 
 const BagTile = (props) => {
 
   return (
     <div>
       <ul>
-        <li>{props.bag.name}</li>
+        <li><Link to={`/bag/${props.bag.id}`}>{props.bag.name}</Link></li>
       </ul>
     </div>
   )
